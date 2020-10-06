29cbc8accc6bc99802e0b1a2593248c43b66f715
John <jkiczek@asu.edu>
2020-10-05 22:21:47 -0700

    Documented file with comments

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -3,24 +3,44 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   
+  /**
+   * Constructor for AddingMachine class
+   */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
+  /**
+   * @return the total value
+   */
   public int getTotal () {
     return 0;
   }
   
+  /**
+   * adds passed value to total
+   * @param value
+   */
   public void add (int value) {
   }
 
+  /**
+   * subtracts passes value to total
+   * @param value
+   */
   public void subtract (int value) {
   }
-
+  
+  /**
+   * returns history of all transactions
+   */
   public String toString () {
     return "";
   }
 
+  /**
+   * clears "memory"
+   */
   public void clear() {
   }
 }
